6958f99b110cd86555f09b7d5f02fa0a6395d239
estevo144 <estevo144@gmail.com>
2024-02-24 20:55:38 -0300

    DN-02 feat: Adicionado Arquivo custom.php. Para controle de dados sensiveis.

---
+++ projetoN/projetoN/app/config/custom.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'URL_AUTORIZACAO' => 'https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc',
+    'URL_NOTIFICACAO' => 'https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6',
+    
+];
